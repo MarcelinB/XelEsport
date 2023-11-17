8781c38fb21bc50b9b1decb642e4085967e706d4
m.montembault <marcelin.bertrand-montembault@digivet.fr>
2023-11-17 09:32:49 +0100

    Move the foreach on leagues after benjamin review

--- xelAPI/src/matches/lolMatch.service.ts
+++ xelAPI/src/matches/lolMatch.service.ts
@@ -18,18 +18,10 @@ export class LeagueOfLegendMatchService {
 
   GAME_LEAGUE_OF_LEGEND_ID = 1;
 
-  /**
-   * Retrieve League of Legends matches based on user preferences.
-   *
-   * @param user -A user object.
-   * @returns An array of League of Legends matches based on user preferences.
-   */
   async getLolMatchesByUserPreference(user: User): Promise<any> {
-    //Get all the user preferences
     const { preferenceGames, preferenceLeagues, preferenceTeams } = await this.getUserPreferences(user);
     const allLolMatches = {};
-    
-    // Check if the user wants to see all matches from the game.
+
     if (preferenceGames.find(preferenceGame => preferenceGame.getAllMatchesFromGame === true)) {
       const matches = await this.getAllLolmMatchesSincesThreeWeeks();
       for (const match of matches) {
@@ -37,21 +29,15 @@ export class LeagueOfLegendMatchService {
       }
       return Object.values(allLolMatches);
     } else {
-      // Check if the user wants to see all matches from a specific league.
       if (preferenceLeagues.find(preferenceLeague => preferenceLeague.getAllMatchesFromLeague === true)) {
-        const allPrefferedLeagues = [];
-        preferenceLeagues.forEach(league => {
-          allPrefferedLeagues.push(league.league.name);
-        });
-        const matches = await this.getLolLeaguesMatches(allPrefferedLeagues);
+        const matches = await this.getLolLeaguesMatches(preferenceLeagues);
         for (const match of matches) {
           if (!allLolMatches[match.id]) {
             allLolMatches[match.id] = match;
           }
         }
       } 
-  
-      // Get matches based on the user's team preferences.
+
       const allPrefferedTeams = [];
       preferenceTeams.forEach(team => {
         allPrefferedTeams.push(team.team.name);
@@ -62,16 +48,11 @@ export class LeagueOfLegendMatchService {
           allLolMatches[match.id] = match;
         }
       }
-  
+
       return Object.values(allLolMatches);
     }
   }
-  /**
-   * Query League of Legends matches from Cargo based on a WHERE clause.
-   *
-   * @param whereClause - The WHERE clause for querying matches.
-   * @returns An array of League of Legends matches that match the WHERE clause.
-   */
+
   private async queryMatchesFromCargo(whereClause: string) {
     const cargo = new CargoClient();
     const matches = await cargo.query({
@@ -104,19 +85,11 @@ export class LeagueOfLegendMatchService {
     return lolMatches;
   }
 
-  /**
- * Retrieve League of Legends matches for specific teams within the last three weeks.
- *
- * @param teamNames - An array of team names for which to retrieve matches.
- * @returns An array of League of Legends matches for the specified teams.
- */
   async getLolMatchesByTeamsNames(teamNames: string[]) {
     if (teamNames.length === 0) {
-      // If the array is empty, return an empty result.
       return [];
     }
 
-    // Construct the WHERE clause for all specified team names.
     const whereClause = `MatchSchedule.DateTime_UTC >= DATE("${this.dateService.getThreeWeeksAgoISO()}") AND (` +
       teamNames.map(teamName => `MatchSchedule.Team1="${teamName}" OR MatchSchedule.Team2="${teamName}"`).join(" OR ") +
       ")";
@@ -129,19 +102,17 @@ export class LeagueOfLegendMatchService {
     }
   }
 
-   /**
- * Retrieve League of Legends matches for specific leagues within the last three weeks.
- *
- * @param leaguesNames - An array of team leagues for which to retrieve matches.
- * @returns An array of League of Legends matches for the specified leagues.
- */
-   async getLolLeaguesMatches(leaguesNames: string[]) {
+  async getLolLeaguesMatches(preferenceLeagues: any[]) {
+    const leaguesNames = [];
+    console.log(preferenceLeagues);
+    preferenceLeagues.forEach(league => {
+      leaguesNames.push(league.league.name);
+    });
+
     if (leaguesNames.length === 0) {
-      // If the array is empty, return an empty result.
       return [];
     }
 
-    // Construct the WHERE clause for all specified leagues names.
     const whereClause = `MatchSchedule.DateTime_UTC >= DATE("${this.dateService.getThreeWeeksAgoISO()}") AND (` +
     leaguesNames.map(leagueName => `MatchSchedule.ShownName LIKE "%${leagueName}%"`).join(" OR ") +
     ")";
@@ -153,24 +124,12 @@ export class LeagueOfLegendMatchService {
     }
   }
 
-
-  /**
-   * Retrieve all League of Legends matches within the last three weeks.
-   *
-   * @returns An array of all League of Legends matches within the last three weeks.
-   */
   async getAllLolmMatchesSincesThreeWeeks() {
     return this.queryMatchesFromCargo(
       `MatchSchedule.DateTime_UTC >= DATE("${this.dateService.getThreeWeeksAgoISO()}")`
     );
   }
 
-    /**
-   * Retrieve preferences for a user.
-   * 
-   * @param user A user object.
-   * @returns All the preferences as preferences objects.
-   */
   async getUserPreferences(user: User){
     const preferenceGames = await this.preferenceGameService.findAllByUserId(user.id);
     const preferenceLeagues = await this.preferenceLeagueService.findAllByUserId(user.id);
@@ -182,5 +141,4 @@ export class LeagueOfLegendMatchService {
       preferenceTeams
     }
   }
-
 }
